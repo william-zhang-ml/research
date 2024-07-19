adced2724689e42b0e1ce83837daedf68debb8b4
William Zhang <william.zhang.ml@gmail.com>
2024-07-18 22:57:26 -0400

    add PCA contour function

--- misc.py
+++ misc.py
@@ -26,3 +26,24 @@ def bootstrap(
         sample = np.random.choice(data, size=sample_size, replace=True)
         stats.append(statistic(sample))
     return np.array(stats)
+
+
+def get_pca_contour(data: np.ndarray, stdev: float = 3) -> np.ndarray:
+    """Estimate a data-aligned/centered contour using SVD/PCA/whitening math.
+
+    Args:
+        data (np.ndarray): enough samples to model distribution stats (N, 2)
+        stdev (float): contour value in units of standard deviation
+
+    Returns:
+        np.ndarray: contour (300, 2)
+    """
+    # compute needed data statistics
+    samp_mean = data.mean(axis=0)
+    [_, sing_val, basis] = np.linalg.svd(data - samp_mean)
+
+    # apply data statistics to a circle with <stdev> radius
+    radians = np.linspace(0, 2 * np.pi, 300)
+    circle = stdev * np.stack([np.cos(radians), np.sin(radians)], axis=1)
+    scaled_basis = np.diag(sing_val / np.sqrt(len(data))) @ basis
+    return circle @ scaled_basis + samp_mean
